6a0354c44e58495b648caacca3345c80308f3e59
RiazUr Rahman <riazrahmanur@gmail.com>
2026-06-19 00:02:16 +0600

    replace local API endpoint with production Render URL

--- frontend/app/lib/osrm.tsx
+++ frontend/app/lib/osrm.tsx
@@ -1,8 +1,9 @@
+const API_BASE = 'https://dhaka-route-app-3.onrender.com'
 export async function fetchRoute(
   start: [number, number],
   end: [number, number]
 ) {
-  const url = `http://localhost:8000/route?start_lat=${start[0]}&start_lng=${start[1]}&end_lat=${end[0]}&end_lng=${end[1]}`;
+  const url = `${API_BASE}/route?start_lat=${start[0]}&start_lng=${start[1]}&end_lat=${end[0]}&end_lng=${end[1]}`;
 
   const res = await fetch(url);
   const data = await res.json();
