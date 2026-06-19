00dfa87da8dfe150d639fed6ae754d698b548f72
RiazUr Rahman <riazrahmanur@gmail.com>
2026-06-19 19:52:37 +0600

    update the cors middleware

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 # Allow Next.js frontend to talk to this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
